00330b17e7c4ec10eaea03fdb8c689e552f347a4
Artem Bashkirev <134786179+art-bashkirev@users.noreply.github.com>
2023-11-03 14:26:29 +0000

    incloops: solved

--- incloops/f.java
+++ incloops/f.java
@@ -6,20 +6,20 @@ public class Main {
     public static void main(String[] args) {
         // ...code... lonely here...
         List<Integer> prime = new ArrayList<>();
-        for (int i = 1; i < 50; i++) {
+        System.out.println((2*1)-1);
+        for (int i = 2; i < 50; i++) {
             if (isPrime(i)) {
                 System.out.println((2*i)-1);
             }
         }
-
     }
 
     static boolean isPrime(int num) {
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
-                return true;
+                return false;
             }
         }
-        return false;
+        return true;
     }
 }
